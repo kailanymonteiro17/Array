acf80a6bc6ce8e74b0ca4e51b4118dd2c4c2331d
kailanymonteiro17 <kailany.monteiro01@icloud.com>
2025-04-03 09:44:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 
 var nomes = ["kailany","samuel","elias","maria"]
 
-var nomeEscolhido = parseInt(pronpt("digite um numero de 0 a 4"))
+var nomeEscolhido = parseInt(prompt("digite um numero de 0 a 4"))
 
 document.write("nome escolhido: " + nomes[0])
