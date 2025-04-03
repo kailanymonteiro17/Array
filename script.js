806b33548995294b5d49dd3ea834a2ff18cfe962
kailanymonteiro17 <kailany.monteiro01@icloud.com>
2025-04-03 09:37:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//JS - Javascript
+
+var nomes = ["kailany","samuel","elias","maria"]
+
+var nomeEscolhido = parseInt(pronpt("digite um numero de 0 a 4"))
+
+document.write("nome escolhido: " + nomes[0])
